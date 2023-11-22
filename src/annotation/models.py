bde6d4a83a64e8c696b3a94c902838730a606fc5
cooi0004 <cooi0004@student.monash.edu>
2023-11-22 13:13:22 +1100

    fix: column naming

--- src/annotation/models.py
+++ src/annotation/models.py
@@ -9,18 +9,22 @@ Base = declarative_base()
 class Highlight(Base):
     __tablename__ = 'HIGHLIGHTS'
     id = Column(String(36), primary_key=True)
-    start_meta = Column(JSON)
-    end_meta = Column(JSON)
+    startMeta = Column(JSON)
+    endMeta = Column(JSON)
     text = Column(String(255))
     url = Column(String(255))
-    annotation_tag = Column(Enum(AnnotationTag))
+    annotationTag = Column(Enum(AnnotationTag))
     notes = Column(String(255))
+    unit = Column(String(10), ForeignKey('UNITS.unitCode'))
 
 class AnnotationActionPoint(Base):
     __tablename__ = 'ACTIONS'
     id = Column(Integer, primary_key=True)
     action = Column(String(255))
-    actionpoint = Column(Enum(ActionPointCategory))
+    actionPoint = Column(Enum(ActionPointCategory))
     deadline = Column(Date)
-    annotation_id = Column(String(255), ForeignKey('HIGHLIGHTS.id'))
+    annotationId = Column(String(255), ForeignKey('HIGHLIGHTS.id'))
 
+class Unit(Base):
+    __tablename__ = 'UNITS'
+    unitCode = Column(String(10), primary_key=True)
