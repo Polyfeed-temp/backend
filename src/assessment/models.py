af0aec8fe483f4ba1992deb02e926e4239cb5f56
cooi0004 <cooi0004@student.monash.edu>
2023-11-27 15:50:05 +1100

    fix: model to autoload from db

--- src/assessment/models.py
+++ src/assessment/models.py
@@ -1,13 +1,11 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from src.database import engine
 from src.base import get_base
 from sqlalchemy.orm import relationship
 
 Base = get_base()
 
 class Assessment(Base):
-    __tablename__ = 'ASSESSMENT'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    unitCode = Column(String(10), ForeignKey('UNIT.unitCode'))
-    assessmentName = Column(String(255))
+    __table__ = Table("ASSESSMENT", Base.metadata, autoload_with=engine)
 
 # may need to change studentId to User.monashId
--- src/user/models.py
+++ src/user/models.py
@@ -1,20 +1,13 @@
-from sqlalchemy import Column, String, Integer, Enum
+from sqlalchemy import Column, String, Integer, Enum, Table
 from .enums import Role
+from src.database import engine
 from src.base import get_base
 
 Base = get_base()
 
 
 class User(Base):
-    __tablename__ = 'USER'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    monashId = Column(String(10))
-    monashObjectId = Column(String(255))
-    authcate = Column(String(255))
-    email = Column(String(255))
-    lastName = Column(String(255))
-    firstName = Column(String(255))
-    role = Column(Enum(Role))
+    __table__ = Table("USER", Base.metadata, autoload_with=engine)
 
 # monashId refers to student or staff ID number
 # monashObjectId is something that can be used as a unique identifier and anchor in the monash system
