c637a893a8c947682211bb2f98e98189e9431a7b
cooi0004 <cooi0004@student.monash.edu>
2023-12-07 02:16:58 +1100

    fix: import issue

--- src/openai/service.py
+++ src/openai/service.py
@@ -1,4 +1,26 @@
-from app import question
+
+from openai import OpenAI
+
+apikey =  "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"
+
+
+def question(role, content):
+    client = OpenAI(api_key=apikey)
+
+    if len(content) >= 18000:
+        print("Message is too long! Consider reducing the size of the message!")
+        # this is an error to be printed out
+    else:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": role, "content": content}
+                # role: system. content: "Compose a poem that explains the concept of recursion in programming."
+            ]
+        )
+        # print(completion.choices[0].message)
+        return completion.choices[0].message
+
 # apikey= "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"
 # client = OpenAI(api_key=apikey)
 
@@ -10,6 +32,6 @@ def chat_completion(prompt):
     # return response.choices[0].text
     return question("system", prompt)
 
-def explain_further(content):
+def explain_further(content:str):
     prompt = "Explain the following content in dot points:\n\n" + content + "\n\nExplaination:"
     return chat_completion(prompt)
