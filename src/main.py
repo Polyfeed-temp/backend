ae7c44ec656ccf42c4ca7bbd1e93926614575d97
cooi0004 <cooi0004@student.monash.edu>
2024-01-19 14:11:57 +1100

    feat: added templates for main page, sign up and guide

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from src.annotation import router as annotation_router
 from src.user import router as user_router
@@ -12,7 +12,9 @@ from src.highlight import router as highlight_router
 from src.action import router as action_router
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.config import Config
-
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from src.dependencies import oauth2_scheme
 
 config = Config('.env')
@@ -20,6 +22,8 @@ config = Config('.env')
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(SessionMiddleware, secret_key=config('RANDOM_SECRET_KEY', cast=str))
 
 app.add_middleware(CORSMiddleware,  allow_origins=["*"],
@@ -38,3 +42,16 @@ app.include_router(action_router.router, prefix="/api/action", tags=["action"])
 app.include_router(login_router.router, prefix="/api/login", tags=["login"])
 app.include_router(openai_router.router, prefix="/api/openai", tags=["openai"])
 
+@app.get("/healthcheck",tags=["health check"])
+def healthcheck():
+    return {"status": "ok"}
+@app.get('/signup', response_class=HTMLResponse)
+def signup(request: Request):
+    return templates.TemplateResponse('signup.html', {'request': request})
+@app.get('/', response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/guide', response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('guide.html', {'request': request})
