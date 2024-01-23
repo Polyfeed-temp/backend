90209498c980b257ee64ece575fee365dd7a54e8
cooi0004 <cooi0004@student.monash.edu>
2024-01-23 23:20:13 +1100

    update config for server dev deployment

---
+++ server.py
@@ -0,0 +1,10 @@
+import uvicorn
+#for development only need to use diffeernt ssl service
+if __name__ == '__main__':
+    uvicorn.run("src.main:app",
+                host="0.0.0.0",
+                port=8000,
+                reload=True,
+                ssl_keyfile="./localhost-key.pem",
+                ssl_certfile="./localhost.pem"
+                )
