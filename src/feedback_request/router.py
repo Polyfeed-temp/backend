da6edf708395e173bb0fd8ae7495e781bd4f3a86
thomas <tommy199469@gmail.com>
2025-03-21 17:44:18 +0800

    add missing function

--- src/feedback_request/router.py
+++ src/feedback_request/router.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from src.database import get_db
 from src.login.service import get_current_user
 from .schemas import FeedbackRequestPydantic
-from .service import create_feedback_request, get_feedback_requests, get_feedback_request_by_assignment
+from .service import create_feedback_request, get_feedback_requests, get_feedback_request_by_assignment,get_feedback_request_by_unitcode_assessment
 
 router = APIRouter()
 
