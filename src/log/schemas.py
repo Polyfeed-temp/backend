035ab84ce3eb2a0de3c7d9dcfdc28369ad223a54
thomas <tommy199469@gmail.com>
2025-09-30 22:24:37 +1000

    change the url size

--- src/log/schemas.py
+++ src/log/schemas.py
@@ -4,7 +4,7 @@ from pydantic import UUID4, BaseModel, Field
 class Item(BaseModel):
     eventType: str = None
     content: Union[str, None] = None
-    baseUrl: str = None
+    baseUrl: str = Field(None, max_length=4294967295)  # LONGTEXT max length
     userEmail :str = None
     ipAddress:str= None
     eventSource:str=None
